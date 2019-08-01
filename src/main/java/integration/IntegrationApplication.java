635e285e2fd05dc0fa4be273f11954eea73b3458
C Busoi <C Busoi>
2019-08-01 11:54:06 +0200

    sl4j logger and app.processing.entry point as property in dev

--- src/main/java/integration/IntegrationApplication.java
+++ src/main/java/integration/IntegrationApplication.java
@@ -1,12 +1,10 @@
 package integration;
 
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import javax.print.attribute.standard.MediaSize.Other;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.camel.Exchange;
@@ -15,6 +13,8 @@ import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -35,7 +35,7 @@ ab -n 1 -c 1 -T 'Content-Type: application/json' -p ./bin/post.array.txt http://
 */
 @SpringBootApplication
 @EnableConfigurationProperties
-@EnableCaching 
+@EnableCaching
 @EntityScan(basePackages = { "integration.model" })
 public class IntegrationApplication {
 	@Value("${server.port}")
@@ -48,6 +48,8 @@ public class IntegrationApplication {
 	int sizeQueue;
 
 	int sizePool = Runtime.getRuntime().availableProcessors();
+
+	private static final Logger logger = LoggerFactory.getLogger(IntegrationApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntegrationApplication.class, args);
@@ -73,6 +75,7 @@ public class IntegrationApplication {
 						.stream().map(Object::toString).collect(Collectors.joining("\n"));
 				Message msg = exchange.getOut();
 				msg.setBody(infoStr, String.class);
+				logger.error("error response", rootEx);
 			}
 
 			private Throwable getRootCause(Throwable e) {
@@ -82,68 +85,66 @@ public class IntegrationApplication {
 				return e;
 			}
 		}
+				
+		@Value("${app.processing.entry:seda:input}")
+		String entryProcessing;
 
 		@SuppressWarnings("deprecation")
 		@Override
 		public void configure() {
-			
+
 			errorHandler(defaultErrorHandler().maximumRedeliveries(0));
+
+			onException(org.apache.camel.CamelAuthorizationException.class).handled(true)
+					.transform(simple("Access Denied with the Policy of ${exception.policyId} !"))
+					.setHeader(Exchange.HTTP_RESPONSE_CODE, simple("401"));
 			
-			onException(Exception.class).handled(true).maximumRedeliveries(0)
-					.transform()
-					.simple("${date:now:yyyy-MM-dd HH:mm:ssZ} -- ${body}")
-					.bean(PrepareErrorResponse.class)
-					.multicast().to("{{app.error.log}}").to("log:integration.LOG?level=ERROR")
+			onException(Exception.class).handled(true).maximumRedeliveries(0).transform()
+					.simple("${date:now:yyyy-MM-dd HH:mm:ssZ} -- ${body}").bean(PrepareErrorResponse.class).multicast()
+					.to("{{app.error.log}}").to("log:integration.LOG?level=ERROR")
 					.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
-					.setFaultHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
-					.end();
+					.setFaultHeader(Exchange.HTTP_RESPONSE_CODE, constant(400)).end();
 
 			restConfiguration().contextPath(contextPath).port(serverPort).enableCORS(true).apiContextPath("/api-doc")
 					.apiProperty("api.title", "Integration API").apiProperty("api.version", "v1")
 					.apiProperty("cors", "true").apiContextRouteId("doc-api").component("servlet")
 					.bindingMode(RestBindingMode.json).dataFormatProperty("prettyPrint", "true");
 
-			final String stagedInput = "seda:input";
-			final String persistence = "bean:persistenceService?method=persist(${body})";
-			final String remotePersistence = "direct:remote-persistence";
-
-			rest("/").produces(MediaType.APPLICATION_JSON).consumes(MediaType.APPLICATION_JSON).enableCORS(true)
-				.post("/")
-					.description("POST an entity whose mapping state is unknown, with the intent to be persisted.")
-					.route().routeId("direct")
-					.inputType(Map.class)
-					.choice()
-						.when(xpath("payload/item"))
-						.to("")
-					.end()
-					.to(stagedInput)
-					.endRest()
-				.post("/all")
-					.description(
-							"POST an array of entites whose mapping states is unknown, with the intent to be all persisted.")
-					.route().routeId("direct-array")
-					.inputType(List.class)
-					.split(body())
-					.to(stagedInput)
-					.endRest()
-				.post("/{type}")
-					.description(
-							"POST an entity of dynamic {type}, to be persistCalendar.getInstance().getTime().toGMTString()+\"$\"ed according to its JPA mappings.")
-					.route().routeId("direct-mapped").inputType(Map.class)
-					.to("bean:integrationConverter?method=toPersistent(${header.type},${body})")
-					.to(stagedInput);
-
-			from(stagedInput)
-				.threads(sizePool).maxQueueSize(sizeQueue)
-				.to(remotePersistence);
+			final String STAGED_INPUT = "seda:input";
+			final String REMOTE_PERSISTENCE = "direct:remote-persistence";
+			final String LOCAL_PERSISTENCE = "direct:local-persistence";
+			final String PERSISTENCE = "bean:persistenceService?method=persist(${body})";
 			
-			from(remotePersistence)
-				.hystrix()
-					.to(persistence)
-		        .onFallback()
-		            .transform().simple("FALLBACK Hystrix ${body}")
-		        	.log("${body}")
-		        .end();
+			rest("/").produces(MediaType.APPLICATION_JSON).consumes(MediaType.APPLICATION_JSON).enableCORS(true)
+					.post("/")
+						.description("POST an entity whose mapping state is unknown, with the intent to be persisted.")
+						.route().routeId("direct").inputType(Map.class)
+						.choice()
+							.when(xpath("payload/item"))
+								.to("log:integration.LOG?level=INFO")
+							.end()
+						.to(entryProcessing)
+						.endRest()
+					.post("/all")
+						.description(
+								"POST an array of entites whose mapping states is unknown, with the intent to be all persisted.")
+						.route().routeId("direct-array").inputType(List.class)
+						.split(body())
+						.to(entryProcessing)
+						.endRest()
+					.post("/{type}")
+						.description(
+								"POST an entity of dynamic {type}, to be persistCalendar.getInstance().getTime().toGMTString()+\"$\"ed according to its JPA mappings.")
+						.route().routeId("direct-mapped").inputType(Map.class)
+						.to("bean:integrationConverter?method=toPersistent(${header.type},${body})")
+						.to(entryProcessing);
+
+			from(STAGED_INPUT).threads(sizePool).maxQueueSize(sizeQueue).to(REMOTE_PERSISTENCE);
+
+			from(REMOTE_PERSISTENCE).hystrix().to(LOCAL_PERSISTENCE).onFallback().transform()
+					.simple("FALLBACK Hystrix ${body}").log("${body}").end();
+
+			from(LOCAL_PERSISTENCE).to(PERSISTENCE).end();
 		}
 
 	}
